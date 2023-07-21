e7aec3aca5ed3393c2f91fa5b31717f8d9d2ebb2
Andrey Osipov <mitol5549@gmail.com>
2023-07-21 17:48:01 +0200

    stored Loader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import HomePage from './pages/Home';
-import EventsPage from './pages/Events';
+import EventsPage, { eventsLoader } from './pages/Events';
 import NewEventPage from './pages/NewEvent';
 import EventDetailPage from './pages/EventDetail';
 import EditEventPage from './pages/EditEvent';
@@ -46,16 +46,7 @@ const router = createBrowserRouter([
           {
             index: true,
             element: <EventsPage />,
-            loader: async () => {
-              const response = await fetch('http://localhost:8080/events');
-
-              if (!response.ok) {
-                // ...
-              } else {
-                const resData = await response.json();
-                return resData.events;
-              }
-            },
+            loader: eventsLoader,
           },
           {
             path: 'new',
