614b70f529796d86b69b77491e10b1aee745dfc5
Andrey Osipov <mitol5549@gmail.com>
2023-07-24 12:43:19 +0200

    fixs

--- frontend/src/pages/EventsLayout.js
+++ frontend/src/pages/EventsLayout.js
@@ -1,12 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet /* useNavigation */ } from 'react-router-dom';
 
 import EventsNavigation from '../components/EventsNavigation';
 
 const EventsLayout = () => {
+  // const navigation = useNavigation();
+
   return (
     <>
       <EventsNavigation />
       <main>
+        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
         <Outlet />
       </main>
     </>
